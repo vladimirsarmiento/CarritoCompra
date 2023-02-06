f434a393a495c93c0f137b95ecaf4dd22eb2ddc9
Vladimir <vladimir.sarmiento.27@unsch.edu.pe>
2023-02-05 22:11:17 -0500

    Modificaciones y cambios en config LADO ADMIN

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -6,64 +6,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
-//import { InicioComponent } from './components/inicio/inicio.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+//login
 import { LoginComponent } from './components/login/login.component';
-import { IndexClienteComponent } from './components/clientes/index-cliente/index-cliente.component';
+//config
+import { ConfigComponent } from './components/config/config.component';
+//boostrap
 import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
-import { CreateClienteComponent } from './components/clientes/create-cliente/create-cliente.component';
-import { EditClienteComponent } from './components/clientes/edit-cliente/edit-cliente.component';
-import { CreateProductoComponent } from './components/productos/create-producto/create-producto.component';
+//ngx
 import { NgxTinymceModule } from 'ngx-tinymce';
+//products
+import { CreateProductoComponent } from './components/productos/create-producto/create-producto.component';
 import { IndexProductoComponent } from './components/productos/index-producto/index-producto.component';
 import { UpdateProductoComponent } from './components/productos/update-producto/update-producto.component';
 import { InventarioProductoComponent } from './components/productos/inventario-producto/inventario-producto.component';
-import { CreateCuponComponent } from './components/cupones/create-cupon/create-cupon.component';
-import { IndexCuponComponent } from './components/cupones/index-cupon/index-cupon.component';
-import { UpdateCuponComponent } from './components/cupones/update-cupon/update-cupon.component';
-import { ConfigComponent } from './components/config/config.component';
 import { VariedadProductoComponent } from './components/productos/variedad-producto/variedad-producto.component';
 import { GaleriaProductoComponent } from './components/productos/galeria-producto/galeria-producto.component';
-
-import { CreateDescuentoComponent } from './components/descuento/create-descuento/create-descuento.component';
-import { EditDescuentoComponent } from './components/descuento/edit-descuento/edit-descuento.component';
-import { IndexDescuentoComponent } from './components/descuento/index-descuento/index-descuento.component';
 import { ReviewsProductoComponent } from './components/productos/reviews-producto/reviews-producto.component';
+//ventas
 import { IndexVentasComponent } from './components/ventas/index-ventas/index-ventas.component';
 import { DetalleVentasComponent } from './components/ventas/detalle-ventas/detalle-ventas.component';
-/*import { VentasSofwareComponent } from './components/ventas-sofware/ventas-sofware.component';*/
-import { IndexCuentasComponent } from './components/cuentas/index-cuentas/index-cuentas.component';
-import { CreateCuentasComponent } from './components/cuentas/create-cuentas/create-cuentas.component';
-import { EditCuentasComponent } from './components/cuentas/edit-cuentas/edit-cuentas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    //InicioComponent,
     SidebarComponent,
     LoginComponent,
-    IndexClienteComponent,
-    CreateClienteComponent,
-    EditClienteComponent,
     CreateProductoComponent,
     IndexProductoComponent,
     UpdateProductoComponent,
     InventarioProductoComponent,
-    CreateCuponComponent,
-    IndexCuponComponent,
-    UpdateCuponComponent,
     ConfigComponent,
     VariedadProductoComponent,
     GaleriaProductoComponent,
-    CreateDescuentoComponent,
-    EditDescuentoComponent,
-    IndexDescuentoComponent,
     ReviewsProductoComponent,
     IndexVentasComponent,
     DetalleVentasComponent,
-    IndexCuentasComponent,
-    CreateCuentasComponent,
-    EditCuentasComponent
   ],
   imports: [
     BrowserModule,
--- admin/src/app/app.routing.ts
+++ admin/src/app/app.routing.ts
@@ -1,44 +1,29 @@
 import { Routes, RouterModule, CanActivate } from '@angular/router';
 import { ModuleWithProviders } from "@angular/core"
-//import { InicioComponent } from './components/inicio/inicio.component';
-import { LoginComponent } from './components/login/login.component';
 import { AdminGuard } from "./guards/admin.guard";
-import { IndexClienteComponent } from './components/clientes/index-cliente/index-cliente.component';
-import { CreateClienteComponent } from './components/clientes/create-cliente/create-cliente.component';
-import { EditClienteComponent } from './components/clientes/edit-cliente/edit-cliente.component';
+
+//login
+import { LoginComponent } from './components/login/login.component';
+//config
+import { ConfigComponent } from './components/config/config.component';
+//products
 import { CreateProductoComponent } from './components/productos/create-producto/create-producto.component';
 import { IndexProductoComponent } from './components/productos/index-producto/index-producto.component';
 import { UpdateProductoComponent } from './components/productos/update-producto/update-producto.component';
 import { InventarioProductoComponent } from './components/productos/inventario-producto/inventario-producto.component';
-import { CreateCuponComponent } from './components/cupones/create-cupon/create-cupon.component';
-import { IndexCuponComponent } from './components/cupones/index-cupon/index-cupon.component';
-import { UpdateCuponComponent } from './components/cupones/update-cupon/update-cupon.component';
-import { ConfigComponent } from './components/config/config.component';
 import { VariedadProductoComponent } from './components/productos/variedad-producto/variedad-producto.component';
 import { GaleriaProductoComponent } from './components/productos/galeria-producto/galeria-producto.component';
-import { IndexDescuentoComponent } from './components/descuento/index-descuento/index-descuento.component';
-import { CreateDescuentoComponent } from './components/descuento/create-descuento/create-descuento.component';
-import { EditDescuentoComponent } from './components/descuento/edit-descuento/edit-descuento.component';
 import { ReviewsProductoComponent } from './components/productos/reviews-producto/reviews-producto.component';
+//ventas
 import { IndexVentasComponent } from './components/ventas/index-ventas/index-ventas.component';
 import { DetalleVentasComponent } from './components/ventas/detalle-ventas/detalle-ventas.component';
 
-import { IndexCuentasComponent } from './components/cuentas/index-cuentas/index-cuentas.component';
-import { CreateCuentasComponent } from './components/cuentas/create-cuentas/create-cuentas.component';
-import { EditCuentasComponent } from './components/cuentas/edit-cuentas/edit-cuentas.component';
 
 const appRoute: Routes = [
-
     {path: '', redirectTo: 'inicio', pathMatch: 'full'},
 
-    //{path: 'inicio', component: InicioComponent, canActivate: [AdminGuard]},
-
     {path: 'panel', children: [
-        {path: 'clientes', component: IndexClienteComponent, canActivate: [AdminGuard]},
-        {path: 'clientes/registro', component: CreateClienteComponent, canActivate: [AdminGuard]},
-        {path: 'clientes/:id', component: EditClienteComponent, canActivate: [AdminGuard]},
-
-
+        //products
         {path: 'productos', component: IndexProductoComponent, canActivate: [AdminGuard]},
         {path: 'productos/registro', component: CreateProductoComponent, canActivate: [AdminGuard]},
         {path: 'productos/:id', component: UpdateProductoComponent, canActivate: [AdminGuard]},
@@ -46,28 +31,13 @@ const appRoute: Routes = [
         {path: 'productos/variedades/:id', component: VariedadProductoComponent, canActivate: [AdminGuard]},
         {path: 'productos/galeria/:id', component: GaleriaProductoComponent, canActivate: [AdminGuard]},
         {path: 'productos/reviews/:id', component: ReviewsProductoComponent, canActivate: [AdminGuard]},
-
-        {path: 'cupones', component: IndexCuponComponent, canActivate: [AdminGuard]},
-        {path: 'cupones/registro', component: CreateCuponComponent, canActivate: [AdminGuard]},
-        {path: 'cupones/:id', component: UpdateCuponComponent, canActivate: [AdminGuard]},
-
-        {path: 'descuentos', component: IndexDescuentoComponent, canActivate: [AdminGuard]},
-        {path: 'descuentos/registro', component: CreateDescuentoComponent, canActivate: [AdminGuard]},
-        {path: 'descuentos/:id', component: EditDescuentoComponent, canActivate: [AdminGuard]},
-
+        //ventas
         {path: 'ventas', component: IndexVentasComponent, canActivate: [AdminGuard]},
         {path: 'ventas/:id', component: DetalleVentasComponent, canActivate: [AdminGuard]},
-
-        /*{path: 'ventas-software', component: VentasSofwareComponent, canActivate: [AdminGuard]},*/
-
-
-        {path: 'cuentas', component: IndexCuentasComponent, canActivate: [AdminGuard]},
-        {path: 'cuentas/registro', component: CreateCuentasComponent, canActivate: [AdminGuard]},
-        {path: 'cuentas/:id', component: EditCuentasComponent, canActivate: [AdminGuard]},
-
+        //config
         {path: 'configuraciones', component: ConfigComponent, canActivate: [AdminGuard]},
     ]},
-
+    //login
     {path: 'login', component: LoginComponent}
 ]
 
